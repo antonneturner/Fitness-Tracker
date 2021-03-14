4efa002bde7017b3d3eecbb7a63a8e920970a6bf
winfrom within <antonneturner@gmail.com>
2021-03-14 00:19:45 -0500

    add router for index.html and stats html

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -7,3 +7,11 @@ router.get("/", function (req, res) {
 });
 
 module.exports = router;
+
+var router = require("express").Router();
+
+var path = require("path");
+
+router.get("/stats", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
