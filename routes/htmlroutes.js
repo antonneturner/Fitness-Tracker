6a391af26df2b26774e161a03f028a469633c1fc
winfrom within <antonneturner@gmail.com>
2021-03-14 00:31:42 -0500

    delete article file, add exercise route

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -15,3 +15,11 @@ var path = require("path");
 router.get("/stats", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
+
+var router = require("express").Router();
+
+var path = require("path");
+
+router.get("/exercise", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
